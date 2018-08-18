3f429103f183e88319143150cd4680ad35bc7588
村田彩 <murataaya@murataayanomacbook-pro.local>
2018-08-18 16:27:25 +0900

    Initial Commit

---
+++ かっぱへびせん/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  かっぱへびせん
+//
+//  Created by 村田彩 on 2018/08/18.
+//  Copyright © 2018年 村田彩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
