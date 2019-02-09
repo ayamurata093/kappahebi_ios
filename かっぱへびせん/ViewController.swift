0605161edfb12c399589cf7c6aced3c31ecaed4f
村田彩 <murataaya@murataayanoMacBook-Pro.local>
2019-02-09 18:09:25 +0900

    firstios

--- かっぱへびせん/ViewController.swift
+++ かっぱへびせん/ViewController.swift
@@ -1,4 +1,4 @@
-//
+//  コメントアウトはスラッシュ２本　アイス専用のインスタ開設しましたよ
 //  ViewController.swift
 //  かっぱへびせん
 //
@@ -6,20 +6,28 @@
 //  Copyright © 2018年 村田彩. All rights reserved.
 //
 
-import UIKit
+import UIKit        //この行は絶対消さないで！
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var hannyaimg: UIImageView!
+    @IBOutlet weak var worklabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.worklabel.isHidden = true //隠す文字
+        self.hannyaimg.isHidden = true //隠す画像
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
+    @IBAction func tapbtn(_ sender: Any) {
+        self.worklabel.isHidden = false //隠す文字　アクション
+        self.hannyaimg.isHidden = false //画像隠す　アクション
+    }
 }
 
